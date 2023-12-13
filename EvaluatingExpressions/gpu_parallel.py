47e12b2ed03ecb12702a0f112947ae906bbb7b3a
Daniel Lopes <danielgoncalo2002@gmail.com>
2023-12-13 05:14:59 +0000

    Siguraaaateeee

--- EvaluatingExpressions/gpu_parallel.py
+++ EvaluatingExpressions/gpu_parallel.py
@@ -4,11 +4,12 @@ import pycuda.driver as cuda
 import pycuda.autoinit
 from pycuda.compiler import SourceModule
 
-# Read CSV file and functions
+
 df = pd.read_csv("data.csv")
 functions = [line.strip() for line in open("functions.txt").readlines()]
+num_functions = len(functions)
+num_rows = len(df)
 
-# Replace math functions and data columns in the expression
 def process_line(line):
     for u in ["sinf", "cosf", "tanf", "sqrtf", "expf"]:
         line = line.replace(u, f"np.{u[:-1]}")
@@ -16,61 +17,51 @@ def process_line(line):
         line = line.replace(f"_{c}_", f"(df['{c}'].values)")
     return line
 
-# Generate CUDA kernel code dynamically from functions.txt
-def generate_kernel_code():
-    kernel_code = """
-    __device__ float evaluate_expression(float *data, int length, const char* expression) {
-        // Evaluate expression using data values
-        // Code for evaluating the expression goes here...
-    }
-    """
-    # Generate code for each function and append it to kernel_code
+kernel_code = f"""
+__global__ void evaluate_functions(float *output, float *input_data, int num_rows, int num_functions) {{
+    int idx = threadIdx.x + blockIdx.x * blockDim.x;
 
-    # Example code to evaluate the provided functions
-    for i, function in enumerate(functions):
-        function_code = f"""
-        if (blockIdx.x == {i}) {{
-            // Evaluate function {i}
-            // You'll need to implement how to parse and evaluate the function
-            // Use process_line() to process the function before evaluation
-            float result = evaluate_expression(data, length, "{process_line(function)}");
-            results[blockIdx.x] = result;  // Store result
+    if (idx < num_functions) {{
+        final int id = idx;
+        float result = 0.0f;
+        for (int i = 0; i < num_rows; ++i) {{
+            float val = {process_line(functions[id])};
+            result += val;
         }}
-        """
-        kernel_code += function_code
+        output[id] = result / num_rows;  // Calculate mean for each function
+    }}
+}}
+"""
 
-    return kernel_code
 
-# Compile CUDA kernel
-module = SourceModule(generate_kernel_code())
-evaluate_function = module.get_function("evaluate_expression")
+mod = SourceModule(kernel_code)
+evaluate_functions = mod.get_function("evaluate_functions")
 
-# Prepare data for GPU
-data = np.array(df.drop(columns='y'), dtype=np.float32)
-target = np.array(df['y'], dtype=np.float32)
+input_data = np.zeros((num_rows, len(df.columns) - 1), dtype=np.float32)
+output_data = np.zeros(num_functions, dtype=np.float32)
 
-# Allocate GPU memory
-data_gpu = cuda.mem_alloc(data.nbytes)
-target_gpu = cuda.mem_alloc(target.nbytes)
 
-# Transfer data to GPU
-cuda.memcpy_htod(data_gpu, data)
-cuda.memcpy_htod(target_gpu, target)
+for i, column in enumerate(df.columns[:-1]): 
+    input_data[:, i] = df[column].values.astype(np.float32)
 
-# Define grid and block dimensions
-block_dim = (1, 1, 1)  # Adjust as needed
-grid_dim = (len(functions), 1)
+input_data_gpu = cuda.mem_alloc(input_data.nbytes)
+output_data_gpu = cuda.mem_alloc(output_data.nbytes)
 
-# Launch kernel
-evaluate_function(data_gpu, len(df), np.int32(len(df.columns)), target_gpu, block=block_dim, grid=grid_dim)
+cuda.memcpy_htod(input_data_gpu, input_data)
+cuda.memcpy_htod(output_data_gpu, output_data)
 
-# Copy results back from GPU
-results = np.empty(len(functions), dtype=np.float32)
-cuda.memcpy_dtoh(results, results_gpu)
+block_size = 256
+grid_size = (num_functions + block_size - 1) // block_size
 
-# Find minimum Mean Square Error
-best_index = np.argmin(results)
-best_score = results[best_index]
-best_function = functions[best_index]
+evaluate_functions(output_data_gpu, input_data_gpu, np.int32(num_rows), np.int32(num_functions),
+                   block=(block_size, 1, 1), grid=(grid_size, 1))
+
+cuda.memcpy_dtoh(output_data, output_data_gpu)
+
+mse_values = np.square(output_data - df['y'].values).mean(axis=0)
+
+best_score_index = np.argmin(mse_values)
+best_score = mse_values[best_score_index]
+best_function = functions[best_score_index]
 
 print(f"Best score: {best_score} | Best function: {best_function}")
